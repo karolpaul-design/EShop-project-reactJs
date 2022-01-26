a0102c66a1285b4614ff9048292c74b83d26e820
karolpaul-design <pavelchycherin@gmail.com>
2022-01-27 01:16:08 +0200

    created cart-item component and connected our cartdropdown with cartItems reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({ cartItems }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      {/* {cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))} */}
+const CartDropdown = () => {
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => {
+    const { cart } = state;
+    return cart.cartItems;
+  });
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
-    <CustomButton>GO TO CHECKOUT</CustomButton>
-  </div>
-);
+  );
+};
 
-// const mapStateToProps = ({ cart: { cartItems } }) => ({
-//   cartItems,
-// });
-// connect(mapStateToProps)
 export default CartDropdown;
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import CartIcon from "../cart-icon/cart-icon.component";
 import "./cart-item.styles.scss";
 
-const CartItem = ({ item: { imageUrl, price, name } }) => (
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   <div className="cart-item">
     <img src={imageUrl} alt="item" />
     <div className="item-details">
       <span className="name">{name}</span>
-      <span className="price">{/* {quantity} x ${price} */}</span>
+      <span className="price">
+        {quantity} x ${price}
+      </span>
     </div>
   </div>
 );
