9408af4bc59e825874f0eda240c616d627ed1624
karolpaul-design <pavelchycherin@gmail.com>
2022-01-27 00:56:31 +0200

    updated shop data with new data, updated cart utils to group items in cart reduce

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
+import { useSelector, useDispatch } from "react-redux";
+import { addItem } from "../../redux/cart/cart.action";
 
-const CollectionItem = ({ id, name, imageUrl, price }) => (
-  <div className="collection-item" key={id}>
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    ></div>
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+function CollectionItem({ item }) {
+  const { id, name, imageUrl, price } = item;
+
+  // const collection = useSelector((state) => {
+  //   const { cart } = state;
+  //   return cart;
+  // });
+  // console.log("collection > ", collection);
+  const dispatch = useDispatch();
+  const handleAdd = () => {
+    dispatch(addItem(item));
+  };
+  return (
+    <div className="collection-item" key={id}>
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={handleAdd} inverted>
+        ADD to cart
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+}
 
 export default CollectionItem;
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,9 +8,9 @@ const CollectionPreview = ({ id, title, items }) => (
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
-        .filter((item, index) => index < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps}></CollectionItem>
+        .filter((items, index) => index < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item}></CollectionItem>
         ))}
     </div>
   </div>
